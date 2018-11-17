5e545a3f2104486c41702fc73224ebb601954d91
Picori001 <44627981+Picori001@users.noreply.github.com>
2018-11-17 08:56:41 -0800

    Add files via upload
    I'm not sure if the meta files are needed or not, but having all of these together should work.
    I modified the testmap scene by adding a character, player, and status script. In this scene, a HUD should appear and a health bar is used as a pseudo player. You can move the "player" around with WASD, lower and raise the health with IO, and do the same for the mana with KL.

---
+++ Character.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Abstract class for a character (Player, Enemy, NPC, etc.)
+public abstract class Character : MonoBehaviour {
+
+    //Speed and Direction for movement
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody2D body;
+
+    protected Vector2 direction;
+
+    // Use this for initialization
+    protected virtual void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+	}
+
+    private void FixedUpdate()
+    {
+        //Calls the Move function
+        Move();
+    }
+
+    public void Move()
+    {
+        //Moves character based on speed and direction
+        body.velocity = direction.normalized * speed;
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character {
+
+    [SerializeField]
+    private Status health;
+
+    [SerializeField]
+    private Status mana;
+
+    private float maxMana = 100;
+    private float maxHealth = 100;
+
+	// Use this for initialization
+	protected override void Start () {
+
+        //Initializes health and mana values
+        health.Initialize(maxHealth, maxHealth);
+        mana.Initialize(maxMana, maxMana);
+
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+        //Calls the GetInput function then the Move function from character
+        GetInput();
+        base.Update();
+	}
+
+    //Gets the current direction based on user input
+    private void GetInput()
+    {
+        //Used for debugging Health and Mana values
+        if(Input.GetKeyDown(KeyCode.I))
+            health.MyCurrentValue -= 10;
+        if (Input.GetKeyDown(KeyCode.O))
+            health.MyCurrentValue += 10;
+        if (Input.GetKeyDown(KeyCode.K))
+            mana.MyCurrentValue -= 15;
+        if (Input.GetKeyDown(KeyCode.L))
+            mana.MyCurrentValue += 15;
+
+        //Zeroes out direction
+        direction = Vector2.zero;
+
+        //Finds new direction
+        if(Input.GetKey(KeyCode.W))
+            direction += Vector2.up;
+        if (Input.GetKey(KeyCode.A))
+            direction += Vector2.left;
+        if (Input.GetKey(KeyCode.S))
+            direction += Vector2.down;
+        if (Input.GetKey(KeyCode.D))
+            direction += Vector2.right;
+    }
+}
