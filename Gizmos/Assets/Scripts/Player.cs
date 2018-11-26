17939b9b6dda0120a908abb2deb89021f0168562
Picori001 <janelson10@gmail.com>
2018-11-25 17:14:54 -0800

    Current Build for the Project
    This should be the current build for the project as of 11/25. This build combines Sean's camera with Sam's menues.

---
+++ Gizmos/Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character {
+
+    [SerializeField]
+    private Status health;
+
+    [SerializeField]
+    private Status mana;
+
+    private float maxMana = 100;
+    private float maxHealth = 100;
+
+	// Use this for initialization
+	protected override void Start() {
+
+        //Initializes health and mana values
+        health.Initialize(maxHealth, maxHealth);
+        mana.Initialize(maxMana, maxMana);
+
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	protected override void Update() {
+        //Calls the GetInput function then the Move function from character
+        GetInput();
+        base.Update();
+
+
+        //Sam EDIT:if player HP drops to 0 it triggers death update in the menu system and resets health and mana
+        if (health.MyCurrentValue == 0)
+        {
+            PauseMenu.gameOver = true;
+            health.MyCurrentValue = maxHealth;
+            mana.MyCurrentValue = maxMana;
+        }
+    }
+
+    //Gets the current direction based on user input
+    private void GetInput()
+    {
+        //Used for debugging Health and Mana values
+        if(Input.GetKeyDown(KeyCode.I))
+            health.MyCurrentValue -= 10;
+        if (Input.GetKeyDown(KeyCode.O))
+            health.MyCurrentValue += 10;
+        if (Input.GetKeyDown(KeyCode.K))
+            mana.MyCurrentValue -= 15;
+        if (Input.GetKeyDown(KeyCode.L))
+            mana.MyCurrentValue += 15;
+
+        //Zeroes out direction
+        direction = Vector2.zero;
+
+        //Finds new direction
+        if(Input.GetKey(KeyCode.W))
+            direction += Vector2.up;
+        if (Input.GetKey(KeyCode.A))
+            direction += Vector2.left;
+        if (Input.GetKey(KeyCode.S))
+            direction += Vector2.down;
+        if (Input.GetKey(KeyCode.D))
+            direction += Vector2.right;
+    }
+}
---
+++ Gizmos/Assets/Scripts/Status.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Status : MonoBehaviour {
+
+    private Image content;
+
+    [SerializeField]
+    private float lerpSpeed;
+
+    private float currentFill;
+
+    public float MyMaxValue { get; set; }
+
+    private float currentValue;
+
+    //Property to find the current value for a status and the fill amount
+    public float MyCurrentValue
+    {
+        get
+        {
+            return currentValue;
+        }
+        set
+        {
+            //Health and mana cannot go above the max
+            if (value > MyMaxValue)
+                currentValue = MyMaxValue;
+            //Health and mana cannot go below 0
+            else if (value < 0)
+                currentValue = 0;
+            //Sets the current health and mana value
+            else
+                currentValue = value;
+
+            currentFill = currentValue / MyMaxValue;
+        }
+    }
+
+	// Use this for initialization
+	void Start() {
+        content = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+        //Updates the health or mana bar each frame
+        if (currentFill != content.fillAmount)
+            content.fillAmount = Mathf.Lerp(content.fillAmount, currentFill, Time.deltaTime * lerpSpeed);
+    }
+
+    //Initializes current status using given amounts
+    public void Initialize(float currentValue, float maxValue)
+    {
+        MyMaxValue = maxValue;
+        MyCurrentValue = currentValue;
+    }
+}
