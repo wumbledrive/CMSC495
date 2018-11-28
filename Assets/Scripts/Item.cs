d600f574d2ab92448f26772dd04f8bbe8030912c
Picori001 <janelson10@gmail.com>
2018-11-27 19:44:33 -0800

    Items/Inventory
    Beginning to add scripts for items and inventory systems

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//CreateAssetMenu makes something createable in the Unity Asset Menu (Like creating a folder)
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{
+    //Each Item is a scriptable object with a name and image
+    public string ItemName;
+    public Sprite Icon;
+}
---
+++ Assets/Scripts/ItemSlots.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Each item is stored in a specfic spot in the inventory UI
+public class ItemSlots : MonoBehaviour
+{
+    //The image for the slot itself
+    [SerializeField]
+    private Image image;
+
+    //Gets the item in the slot and sets the item
+    private Item _item;
+    public Item Item
+    {
+        get
+        {
+            return _item;
+        }
+        set
+        {
+            _item = value;
+            if (_item == null)
+                image.enabled = false;
+            else
+            {
+                image.sprite = _item.Icon;
+                image.enabled = true;
+            }
+        }
+    }
+
+    //OnValidate is called when script is loaded or value is changed
+    private void OnValidate()
+    {
+        if (image == null)
+            image = GetComponent<Image>();
+    }
+}
