f19e6f59ee557cac4c7c1ec96fd355141c2e72ab
wumbleDrive <wumbledrive@gmail.com>
2018-12-05 17:04:59 -0500

    New Title/Main Screen
    NEW:
    * Background Images
    * Music Added
    * Runes
    TODO: 
    * Button Functionality
    * Scrolling Background
    * Make runes disappear--only for when controller is being used they indicate which button is currently being selected.

---
+++ Assets/Scripts/Menus/Scroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float scrollSpeed;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(scrollSpeed,0f);
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector2(scrollSpeed, 0f);
+    }
+}
