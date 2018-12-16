e913e77c0ebd0ae0dba36ff8c90055cec05176e0
wumbleDrive <wumbledrive@gmail.com>
2018-12-16 13:07:00 -0500

    Added Widow Resolution Script
    A script for changing window resolution called WindowManager.cs

---
+++ Assets/Scripts/Menus/WindowManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowManager : MonoBehaviour {
+
+    #region Attributes
+    #region Player Pref Key Constants
+
+    private const string RESOLUTION_PREF_KEY = "resolution";
+    #region Resolution
+    [SerializeField]
+    private Text resolutionText;
+    private Resolution[] resolutions;
+    private int currentResolutionIndex = 0;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #region Helpers
+    #region Resolution Cycling
+    private void SetResolutionText(Resolution resolution)
+    {
+        resolutionText.text = resolution.width + "x" + resolution.height;
+    }
+    public void SetNextResolution()
+    {
+        currentResolutionIndex = GetNextWrappedIndex(resolutions, currentResolutionIndex);
+        SetResolutionText(resolutions[currentResolutionIndex]);
+    }
+    public void SetPreviousResolution()
+    {
+        currentResolutionIndex = GetPreviousWrappedIndex(resolutions, currentResolutionIndex);
+        SetResolutionText(resolutions[currentResolutionIndex]);
+    }
+    #endregion
+    #region Index Wrap Helpers
+    private int GetNextWrappedIndex<T>(IList<T> collection, int currentIndex)
+    {
+        if (collection.Count < 1) return 0;
+        return (currentIndex + 1) % collection.Count;
+    }
+    private int GetPreviousWrappedIndex<T>(IList<T> collection, int currentIndex)
+    {
+        if (collection.Count < 1) return 0;
+        if ((currentIndex - 1) < 0) return collection.Count - 1;
+        return (currentIndex - 1) % collection.Count;
+    }
+    #endregion
+    #endregion
+
+}
